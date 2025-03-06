f5496efe71d43d7dd87105143150677771b82be3
JSBHA <jhoan-071995@hotmail.com>
2025-03-05 21:41:02 -0500

    // 3️⃣ Convert a number to binary

---
+++ reto3.js
@@ -0,0 +1,13 @@
+// 3️⃣ Convertir un número a binario
+
+function convertToBinary (n){
+    return n.toString(2)
+
+}
+
+
+
+
+console.log(convertToBinary(25)); // "11001"
+console.log(convertToBinary(50)); // "110010"
+console.log(convertToBinary(100)); // "1100100"
