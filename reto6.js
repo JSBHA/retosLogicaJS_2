408b68c9731587de3a1945705ead8b5ce29d2187
JSBHA <jhoan-071995@hotmail.com>
2025-03-06 12:43:00 -0500

    6️⃣ Find the second longest word in a sentence

---
+++ reto6.js
@@ -0,0 +1,18 @@
+// 6️⃣ Encontrar la segunda palabra más larga en una oración
+
+function secondLongestWord(str) {
+  let words = str
+    .replace(/[^\w\s/]/g, "")
+    .split(" ")
+    .filter((word) => word.length, 0);
+  if (words.length < 2) return null;
+
+  let seconBigWords = words.sort((a, b) => b.length - a.length);
+
+  return seconBigWords[1];
+}
+console.log(secondLongestWord("El desarrollo web es increíble"));
+console.log(secondLongestWord("La programación es una aventura emocionante")); // "emocionante"
+console.log(secondLongestWord("JavaScript es mi lenguaje favorito")); //lenguaje
+console.log(secondLongestWord("Hola mundo!")); // "Hola"
+console.log(secondLongestWord("")); // NULL
