97c8a3b18f3ccdab4a58f96e4cb18e2a50b04fd1
JSBHA <jhoan-071995@hotmail.com>
2025-03-07 14:18:48 -0500

    //1️⃣1️⃣ Detect if a string has all unique characters

---
+++ reto11.js
@@ -0,0 +1,13 @@
+//1️⃣1️⃣ Detectar si una cadena tiene todos los caracteres únicos
+
+function hasUniqueChars(str){
+    let uniqueChar = new Set(str)
+    return uniqueChar.size === str.length
+}
+
+
+console.log(hasUniqueChars("abcdef"));  // true
+console.log(hasUniqueChars("hello"));   // false
+console.log(hasUniqueChars("123456"));  // true
+console.log(hasUniqueChars("aA"));      // true (distingue mayúsculas y minúsculas)
+console.log(hasUniqueChars(""));        // true (cadena vacía)
