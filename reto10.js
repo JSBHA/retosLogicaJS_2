9954af03e2216923e0db528018cb2029a8e0bf8e
JSBHA <jhoan-071995@hotmail.com>
2025-03-07 14:10:40 -0500

    🔟 Find the most frequent element in an array

---
+++ reto10.js
@@ -0,0 +1,25 @@
+// 🔟 Encontrar el elemento más frecuente en un array
+function mostFrequentElement(arr){
+    let frequent = {};
+    let maxFrequent = 0;
+    let mostFrequent = null
+
+    for (let num of arr){
+        frequent[num] = (frequent[num] || 0) + 1 
+
+        if(frequent[num] > maxFrequent){
+            maxFrequent = frequent[num];
+            mostFrequent = num;
+        }
+    }
+    return mostFrequent;
+
+}
+
+
+
+console.log(mostFrequentElement([1, 2, 3, 2, 4, 2, 3]));  // 2
+console.log(mostFrequentElement(["a", "b", "a", "c", "a", "b"]));  // "a"
+console.log(mostFrequentElement([true, false, true, true, false]));  // true
+console.log(mostFrequentElement([5]));  // 5
+console.log(mostFrequentElement([]));  // null (si el array está vacío)
