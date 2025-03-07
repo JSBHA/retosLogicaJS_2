06b079871643c3b34767510d301483e8fae82d0d
JSBHA <jhoan-071995@hotmail.com>
2025-03-06 21:16:13 -0500

    8️⃣ Merge two arrays without duplicate elements

---
+++ reto8.js
@@ -0,0 +1,11 @@
+// 8️⃣ Fusionar dos arrays sin elementos repetidos
+function mergeArrays(arr1, arr2){
+        return [...new Set([...arr1, ...arr2])];
+}
+
+
+console.log(mergeArrays([1, 2, 3], [3, 4, 5])); // [1, 2, 3, 4, 5]
+console.log(mergeArrays(["a", "b", "c"], ["b", "c", "d"])); // ["a", "b", "c", "d"]
+console.log(mergeArrays([10, 20, 30], [30, 40, 50, 10])); // [10, 20, 30, 40, 50]
+console.log(mergeArrays([], [1, 2, 3])); // [1, 2, 3]
+console.log(mergeArrays([], [])); // []
