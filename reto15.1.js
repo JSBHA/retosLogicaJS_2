9b7c3daa383e0e4d2fe270136cc9a331a894393d
JSBHA <jhoan-071995@hotmail.com>
2025-03-10 11:54:28 -0500

    1️⃣5️⃣ **Find the largest number in an array**

---
+++ reto15.1.js
@@ -0,0 +1,10 @@
+// 1️⃣5️⃣ **Encontrar el número más grande en un array**
+
+function findMaxNumber(nums){
+    let maxNumber = Math.max(...nums)
+    return maxNumber
+}
+
+console.log(findMaxNumber([3, 7, 2, 9, 5])); // 9
+console.log(findMaxNumber([-10, -3, -1, -20])); // -1
+console.log(findMaxNumber([100, 50, 200, 150])); // 200
