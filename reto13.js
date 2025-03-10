4240b163f03a8a04c0b7b7f62de9287bafe56d8f
JSBHA <jhoan-071995@hotmail.com>
2025-03-09 22:03:27 -0500

    1️⃣3️⃣ Simulate the FizzBuzz game

---
+++ reto13.js
@@ -0,0 +1,16 @@
+// 1️⃣3️⃣ Simular el juego de "FizzBuzz"
+
+function fizzBuzz(n) {
+  return Array.from({ length: n }, (_, i) => {
+    let num = i + 1;
+    return num % 3 === 0 && num % 5 === 0
+      ? "FizzBuzz"
+      : num % 3 === 0
+      ? "Fizz"
+      : num % 5 === 0
+      ? "Buzz"
+      : num;
+  });
+}
+
+console.log(fizzBuzz(15));
