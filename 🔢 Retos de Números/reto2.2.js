20c61eb5b3ce60bdab2a2ada3573889e9cf7be90
JSBHA <jhoan-071995@hotmail.com>
2025-03-10 13:16:18 -0500

    **2️⃣ Check if a number is a perfect square **

--- 🔢 Retos de Números/reto2.2.js
+++ 🔢 Retos de Números/reto2.2.js
@@ -1,19 +1,19 @@
 // 2️⃣ Verificar si un número es un cuadrado perfecto
 
-function  isPerfectSquare(n){
-    if(n < 0) return false // No hay cuadrados perfectos negativos
-    
-    for (let i = 1; i * i <= n; i++){
-        if (i * i === n){
-            return true; 
-        }
+function isPerfectSquare(n) {
+  if (n < 0) return false; // No hay cuadrados perfectos negativos
+
+  for (let i = 1; i * i <= n; i++) {
+    if (i * i === n) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 console.log(isPerfectSquare(25)); // true
 console.log(isPerfectSquare(16)); // true
 console.log(isPerfectSquare(10)); // false
-console.log(isPerfectSquare(1));  // true
-console.log(isPerfectSquare(0));  // true
+console.log(isPerfectSquare(1)); // true
+console.log(isPerfectSquare(0)); // true
 console.log(isPerfectSquare(36)); // true
