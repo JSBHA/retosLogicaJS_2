7e3ac49260e70c6314cf89149b8eeecdb6926f2c
JSBHA <jhoan-071995@hotmail.com>
2025-03-13 22:11:06 -0500

    **3️⃣ **Contar cuántas veces aparece cada palabra en una oración**

---
+++ 🔡 Retos de Cadenas de Texto/reto3.js
@@ -0,0 +1,22 @@
+// **3️⃣ **Contar cuántas veces aparece cada palabra en una oración**
+
+function countWords(str) {
+  let countWord = {};
+  let words = str
+    .replace(/[^\w\s/]/g, "")
+    .toLowerCase()
+    .split(" ");
+
+  for (let word of words) {
+    if (word) {
+      countWord[word] = (countWord[word] || 0) + 1;
+    }
+  }
+
+  return countWord;
+}
+
+console.log(countWords("Hola mundo, hola a todos.")); // hola: 2, mundo: 1, a: 1, todos: 1
+console.log(countWords("El sol brilla. El cielo es azul.")); //el: 2, sol: 1, brilla: 1, cielo: 1, es: 1
+console.log(countWords("Repetición, repetición, repetición!")); //repeticin: 3
+console.log(countWords("Un solo término.")); //un: 1, solo: 1, trmino: 1
