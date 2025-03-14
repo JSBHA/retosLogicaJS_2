b99af054b835f0b5572b4030cd8926927c7f8a78
JSBHA <jhoan-071995@hotmail.com>
2025-03-13 21:50:27 -0500

    **1️⃣ **Remove all consonants from a string**

---
+++ 🔡 Retos de Cadenas de Texto/reto1.js
@@ -0,0 +1,10 @@
+// 1️⃣ **Eliminar todas las consonantes de una cadena**
+
+function removeConsonants(str) {
+  return str.replace(/[^aeiouAEIOU\s]/g, "");
+}
+
+console.log(removeConsonants("Hola, mundo!")); // "oa uo"
+console.log(removeConsonants("JavaScript es genial")); // "aa i e eia"
+console.log(removeConsonants("PROGRAMACIÓN")); // "OAAIÓ"
+console.log(removeConsonants("1234 hello world!"));
