43677f4da7bcc189c6c66b851c58815c27f22681
JSBHA <jhoan-071995@hotmail.com>
2025-03-19 12:11:40 -0500

    **7️⃣ **Reverse a text string**

---
+++ 🔡 Retos de Cadenas de Texto/reto7.js
@@ -0,0 +1,10 @@
+// **7️⃣ **Invertir una cadena de texto**
+
+function reverseString(str) {
+  return str.split("").reverse().join('');
+}
+
+console.log(reverseString("Hola"));
+// "aloH"
+console.log(reverseString("JavaScript"));
+// "tpircSavaJ"
