ef9b7d31b925e7b5aed0c82560aa9dcf825c7808
JSBHA <jhoan-071995@hotmail.com>
2025-03-19 15:29:19 -0500

    **9️⃣ **Capitalize the first letter of each word**

---
+++ 🔡 Retos de Cadenas de Texto/reto9.js
@@ -0,0 +1,8 @@
+// **9️⃣ **Capitalizar la primera letra de cada palabra**
+
+function capitalizeWords(str) {
+  return str.replace(/\b\w/g, char => char.toUpperCase())
+}
+
+console.log(capitalizeWords("hola mundo")); // "Hola Mundo"
+console.log(capitalizeWords("esto es un reto")); // "Esto Es Un Reto"
