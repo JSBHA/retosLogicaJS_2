94bf15fbe7472458242783145c699a1cd5285dc2
JSBHA <jhoan-071995@hotmail.com>
2025-03-27 15:33:53 -0500

    **9️⃣ **Capitalize the first letter of each word**

--- 🔡 Retos de Cadenas de Texto/reto9.1.js
+++ 🔡 Retos de Cadenas de Texto/reto9.1.js
@@ -1,13 +1,13 @@
 // **9️⃣ **Capitalizar la primera letra de cada palabra**
 
-function capitalizeWords(str){
-    return str.split(' ')
-    .map(word => {
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+function capitalizeWords(str) {
+  return str
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
     })
-    .join(' ')
+    .join(" ");
 }
-
 
 console.log(capitalizeWords("hola mundo")); // "Hola Mundo"
 console.log(capitalizeWords("esto es un reto")); // "Esto Es Un Reto"
